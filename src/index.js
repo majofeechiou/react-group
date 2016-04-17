ce0abfbf4a6d40ef5bff04cd6f3dbcf246498310
Majo Chiou <majo.fee.chiou@gmail.com>
2016-04-17 15:39:44 +0800

    save

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
 // module.exports = require('./base');
 
 // require('./base');
-require('./css/_index');
 // require('./js/RadioGroup');
 
+require('./css/_index');
 module.exports = require('./js/RadioGroup');
 
 // export default class RadioGroup extends React.Component {
